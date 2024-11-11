fda38b2ee690275c24f17c42e4b26fcdd11febb2
rustywhite404 <rustywhite404@gmail.com>
2024-11-11 15:37:18 +0900

    init commit

---
+++ src/main/java/com/sparta/junit5practice/Junit5practiceApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.junit5practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5practiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5practiceApplication.class, args);
+    }
+
+}
